b622d840aa6a0196ed78f571e814578d6eb67bd1
Akshayshahi900 <shahiakshay900@gmail.com>
2025-08-05 10:31:52 +0530

    v0 backend

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,16 +1,83 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
-export class Game{
-    private player1 : WebSocket;
-    private player2 : WebSocket;
-    private board :string;
-    private moves:string[];
-    private startTime :Date;
+import { MOVE, GAME_OVER, INIT_GAME } from "./messages";
 
-    constructor(player1:WebSocket , player2:WebSocket){
-        this.player1= player1;
-        this.player2= player2;
-        this.board ="";
-        this.moves=[];
+export class Game {
+
+    public player1: WebSocket;
+    public player2: WebSocket;
+    public board: Chess;
+    private moveCount=0;
+    private startTime: Date;
+
+    constructor(player1: WebSocket, player2: WebSocket) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess();
         this.startTime = new Date();
+        
+        this.player1.send(JSON.stringify({
+            type:INIT_GAME,
+            payload: {
+                color:"white",
+            }
+        }))
+        this.player2.send(JSON.stringify({
+            type:INIT_GAME,
+            payload: {
+                color:"black",
+            }
+        }))
+    }
+
+    makeMove(socket: WebSocket, move: {
+        from: string;
+        to: string;
+    }) {
+        //validate the type of move using zod
+        console.log(this.board.moves().length);
+        console.log(this.board.board());
+
+        if (this.moveCount % 2 === 0 && socket !== this.player1) {
+            console.log("Early return 1");
+            return
+        }
+        if (this.moveCount % 2 === 1 && socket !== this.player2) {
+            console.log("Early return 2");
+            return;
+        }
+        console.log("Did not early return ");
+        try {
+            this.board.move(move);
+      
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+
+        //check if the game is over
+
+        if(this.board.isGameOver()){
+            this.player1.send(JSON.stringify({
+                type:GAME_OVER,
+                payload:{
+                    winner:this.board.turn() === "w" ? "black" :"white"
+                }
+            }))
+            return;
+        }
+         if(this.moveCount % 2 === 0){
+             this.player2.send(JSON.stringify({
+                 type:MOVE,
+                 payload:move
+             }))
+         }
+         else{
+            this.player1.send(JSON.stringify({
+                types:MOVE,
+                payload:move,
+            }))
+         }
+               this.moveCount++;
     }
 }
--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,11 +1,15 @@
 import { WebSocket } from "ws";
+import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./messages";
 export class GameManager{
     private games :Game[];
-    private pendingUser:WebSocket;
+    private pendingUser:WebSocket | null ;
     private users:WebSocket[];
 
     constructor(){
         this.games = [];
+        this.pendingUser= null;
+        this.users =[];
         
     }
 
@@ -23,12 +27,25 @@ export class GameManager{
         socket.on("message" , (data)=>{
             const message = JSON.parse(data.toString());
 
+
             if(message.type === INIT_GAME){
                 if(this.pendingUser){
                     //start a game
+                    const game = new Game(this.pendingUser , socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+            
+            
                 }
                 else{
                     this.pendingUser = socket;
+                }
+            }
+            if(message.type === MOVE){
+                console.log("Inside move");
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket, message.move);
                 }
             }
         })
